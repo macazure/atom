9d87fa6cbccf1a8a4777c96a439c038c7700f913
Paul Mclean <paul.mclean@capdelivery.gsi.gov.uk>
2021-11-18 13:17:48 +0000

    Update

--- main.tf
+++ main.tf
@@ -4,31 +4,29 @@ provider "aws" {
     profile = "terraform"
 }
 
-resource "aws_s3_bucket" "macbucket" {
-  bucket = "joomla-pm"
+resource "aws_s3_bucket" "atomisesec-remote-state" {
+  bucket = "atomisesec-remote-state"
   acl    = "private"
 
-  tags = {
-    Name        = "joomla-pm"
-  }
 }
-  resource "aws_dynamodb_table" "jooml_locks" {
-  name         = "joomla-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  
+resource "aws_dynamodb_table" "terraform-state-lock" {
+  name             = "terraform-state-locks"
+  billing_mode     = "PROVISIONED"
+  hash_key         = "LockID"
+  read_capacity    = 1
+  write_capacity   = 1  
   attribute {
     name = "LockID"
     type = "S"
   }
-  }
+}
 
-  terraform {
+terraform {
   backend "s3" {
-    bucket         = "joomla-pm"
+    bucket         = "atomisesec-remote-state"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "joomla-running-locks"
+    dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
 }
